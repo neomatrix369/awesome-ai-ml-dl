d6eddf7735fe245e1b2da5f32b4c8bcbd670f713
mani <sadhak001@gmail.com>
2019-03-24 23:21:53 +0000

    Amendments to script to prevent bash error on unbound variables i.e. DOCKER_USER_NAME
    Removed the mention of IMAGE_VERSION when pushing the image to Docker hub in the push-grakn-docker-image-to-hub.sh script

--- examples/data/databases/graph/grakn/push-grakn-docker-image-to-hub.sh
+++ examples/data/databases/graph/grakn/push-grakn-docker-image-to-hub.sh
@@ -6,22 +6,22 @@ set -o pipefail
 
 GRAKN_VERSION=${GRAKN_VERSION:-1.4.3}
 
-if [[ -z "${USER_NAME:-''}" ]]; then
-  read -p "Docker username (must exist on Docker Hub): " USER_NAME
+if [[ -z ${DOCKER_USER_NAME:-""} ]]; then
+  read -p "Docker username (must exist on Docker Hub): " DOCKER_USER_NAME
 fi
 
 IMAGE_NAME=${IMAGE_NAME:-grakn}
 IMAGE_VERSION=${IMAGE_VERSION:-${GRAKN_VERSION}}
-GRAKN_DOCKER_FULL_TAG_NAME="${USER_NAME}/${IMAGE_NAME}"
+GRAKN_DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
 
 findImage() {
 	IMAGE_NAME=$1
 	echo $(docker images ${IMAGE_NAME} -q | head -n1 || true)
 }
 
-IMAGE_FOUND="$(findImage ${USER_NAME}/${IMAGE_NAME})"
+IMAGE_FOUND="$(findImage ${DOCKER_USER_NAME}/${IMAGE_NAME})"
 if [[ -z "${IMAGE_FOUND}" ]]; then
-    echo "Docker image '${USER_NAME}/${IMAGE_NAME}' not found in the local repository"
+    echo "Docker image '${DOCKER_USER_NAME}/${IMAGE_NAME}' not found in the local repository"
     IMAGE_FOUND="$(findImage ${IMAGE_NAME})"
     if [[ -z "${IMAGE_FOUND}" ]]; then
     	echo "Docker image '${IMAGE_NAME}' not found in the local repository"
@@ -30,9 +30,9 @@ if [[ -z "${IMAGE_FOUND}" ]]; then
     	echo "Docker image '${IMAGE_NAME}' found in the local repository"
     fi
 else
-    echo "Docker image '${USER_NAME}/${IMAGE_NAME}' found in the local repository"
+    echo "Docker image '${DOCKER_USER_NAME}/${IMAGE_NAME}' found in the local repository"
 fi
 
 docker tag ${IMAGE_FOUND} ${GRAKN_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION}
-docker login --username=${USER_NAME}
-docker push ${GRAKN_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION}
+docker login --username=${DOCKER_USER_NAME}
+docker push ${GRAKN_DOCKER_FULL_TAG_NAME}
--- examples/data/databases/graph/grakn/runGraknInDocker.sh
+++ examples/data/databases/graph/grakn/runGraknInDocker.sh
@@ -6,13 +6,13 @@ set -o pipefail
 
 GRAKN_VERSION=${GRAKN_VERSION:-1.4.3}
 
-if [[ -z "${USER_NAME:-''}" ]]; then
-  read -p "Docker username (must exist on Docker Hub): " USER_NAME
+if [[ -z ${DOCKER_USER_NAME:-""} ]]; then
+  read -p "Docker username (must exist on Docker Hub): " DOCKER_USER_NAME
 fi
 
 IMAGE_NAME=${IMAGE_NAME:-grakn}
 IMAGE_VERSION=${IMAGE_VERSION:-${GRAKN_VERSION}}
-GRAKN_DOCKER_FULL_TAG_NAME="${USER_NAME}/${IMAGE_NAME}"
+GRAKN_DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
 
 WORKDIR=grakn
 
