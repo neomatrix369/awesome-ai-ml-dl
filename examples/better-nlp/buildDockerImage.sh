cc9e49661f7b234b87830ee66b3da868efad96c1
mani <sadhak001@gmail.com>
2019-03-26 22:48:52 +0000

    Added/updated scripts for building, running and pushing docker images to the docker hub

--- examples/better-nlp/buildDockerImage.sh
+++ examples/better-nlp/buildDockerImage.sh
@@ -1,1 +1,15 @@
-docker build -t better-nlp .
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+if [[ -z ${DOCKER_USER_NAME:-""} ]]; then
+  read -p "Docker username (must exist on Docker Hub): " DOCKER_USER_NAME
+fi
+
+IMAGE_NAME=${IMAGE_NAME:-better-nlp}
+IMAGE_VERSION=${IMAGE_VERSION:-0.1}
+BETTER_NLP_DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
+
+time docker build -t ${BETTER_NLP_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION} .
--- examples/better-nlp/runDockerImage.sh
+++ examples/better-nlp/runDockerImage.sh
@@ -1,5 +1,20 @@
-docker run --rm                        \
-           -it                         \
-           --volume $(pwd):/better-nlp \
-           --workdir /better-nlp       \
-           better-nlp /bin/bash 
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+DOCKER_USER_NAME=${DOCKER_USER_NAME:-"neomatrix369"}
+
+IMAGE_NAME=${IMAGE_NAME:-better-nlp}
+IMAGE_VERSION=${IMAGE_VERSION:-0.1}
+BETTER_NLP_DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
+
+WORKDIR=better-nlp
+
+time docker run --rm                        \
+                --interactive --tty         \
+                --volume $(pwd):/better-nlp \
+                --workdir /${WORKDIR}       \
+                --entrypoint /bin/bash      \
+                ${BETTER_NLP_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION}
