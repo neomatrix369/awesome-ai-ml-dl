41955953e3f0b7abd97704c9a045db0c9a985df6
mani <sadhak001@gmail.com>
2019-03-28 00:55:53 +0000

    Create notebook and move the four examples into it - amend better_nlp library

--- examples/better-nlp/org/neomatrix369/better_nlp.py
+++ examples/better-nlp/org/neomatrix369/better_nlp.py
@@ -21,67 +21,90 @@ class BetterNLP:
 
     minimum_occurrence_frequency = MINIMUM_OCCURRENCE_FREQUENCY
 
-    def __init__(self):
-        self.english_nlp_model = None
+    english_nlp_model = None
 
-    @staticmethod
-    def load_nlp_model():
-        start_time = time.time()
+    model_loading_time = 0
+
+    def load_nlp_model(self):
         # Ensure you do the below two steps before loading the English NLP Model:
         #
         # 	$ python3 -m spacy download en_core_web_lg
         # 	$ python3 -m spacy link en_core_web_lg en
         #
         nlp_model = 'en_core_web_lg'
-        print("Loading model {}...".format(nlp_model))
-        english_nlp_model = spacy.load(nlp_model)
-        end_time = time.time()
-        print("Time taken to load the {} model: {} seconds\n".format(nlp_model, end_time-start_time))
-        return english_nlp_model
 
-    @staticmethod
+        if self.english_nlp_model is None: 
+            print("Loading model {}...".format(nlp_model))
+            start_time = time.time()
+            self.english_nlp_model = spacy.load(nlp_model)
+            self.model_loading_time = time.time() - start_time
+            time_label = "model_loading_time_in_secs"    
+        else:
+            time_label = "model_loading_time_saved_in_secs"
+        
+        result = {}
+        result[time_label] = self.model_loading_time
+        result["model_name"] = nlp_model
+        result["model"] = self.english_nlp_model
+        return result
+
     def token_legend():
-        return ['GPE = Geographic Point Entity', 'FAC = ', 'DATE = calendar date',
+        return ['GPE = Geographic Point Entity', 'FAC = ', 
+                'DATE = calendar date',
                 'NORP = Noun or Pronoun',
                 'PERSON = name of a person (proper noun)',
                 'ORG = Organisation']
 
-    def extract_entities(self, text):
-        if self.english_nlp_model is None:
-            self.english_nlp_model = self.load_nlp_model()
+    def extract_entities(self, model, text):
         start_time = time.time()
-        parsed_text = self.english_nlp_model(text)
-        end_time = time.time()
-        print("Time taken to process the generic text: {} seconds".format(end_time - start_time))
-        print("\n")
-        return parsed_text
+        parsed_text = model(text)
+        duration = time.time() - start_time
 
-    def extract_nouns_chunks(self, text):
-        parsed_generic_text = self.extract_entities(text)
+        result = {}
+        result["parsed_text"] = parsed_text
+        result["extract_entities_processing_time_in_secs"] = duration
+        return result
+
+    def extract_nouns_chunks(self, model, text):
+        parsed_generic_text = self.extract_entities(model, text)
+        parsed_generic_text = parsed_generic_text.get("parsed_text")
         start_time = time.time()
-        chunks = textacy.extract.noun_chunks(parsed_generic_text, min_freq=self.minimum_occurrence_frequency)
-        chunks = map(str, chunks)
-        end_time = time.time()
-        print("Time taken to process the generic text: {} seconds".format(end_time - start_time))
-        print("\n")
-        return map(str.lower, chunks)
+        noun_chunks = textacy.extract.noun_chunks(parsed_generic_text, min_freq=self.minimum_occurrence_frequency)
+        noun_chunks = map(str, noun_chunks)
+        noun_chunks = map(str.lower, noun_chunks)
+        duration = time.time() - start_time
 
-    def extract_facts(self, text, target_topic):
+        result = {}
+        result["parsed_text"] = parsed_generic_text
+        result["noun_chunks"] = noun_chunks
+        result["noun_chunks_processing_time_in_secs"] = duration
+        return result
+
+    def extract_facts(self, model, text, target_topic):
         start_time = time.time()
-        parsed_generic_text = self.extract_entities(text)
-        end_time = time.time()
-        print("Time taken to process the generic text: {} seconds".format(end_time - start_time))
-        print("\n")
-        return textacy.extract.semistructured_statements(parsed_generic_text, target_topic)
+        parsed_generic_text = self.extract_entities(model, text)
+        parsed_generic_text = parsed_generic_text.get("parsed_text")
+        facts = textacy.extract.semistructured_statements(parsed_generic_text, target_topic)
+        duration = time.time() - start_time
 
-    def obfuscate_text(self, text):
+        result = {}
+        result["parsed_text"] = parsed_generic_text
+        result["facts"] = facts
+        result["extract_facts_processing_time_in_secs"] = duration
+        return result
+
+    def obfuscate_text(self, model, text):
+        parsed_generic_text = self.extract_entities(model, text)
+        parsed_generic_text = parsed_generic_text.get("parsed_text")
+
         start_time = time.time()
-        parsed_generic_text = self.extract_entities(text)
-
         for each_entity in parsed_generic_text.ents:
             each_entity.merge()
+        duration = time.time() - start_time
+        
+        result = {}
+        result["parsed_text"] = parsed_generic_text
+        result["obfuscate_text_processing_time_in_secs"] = duration
+        result["obfuscated_text"] = map(obfuscate, parsed_generic_text)
 
-        end_time = time.time()
-        print("Time taken to process the generic text: {} seconds".format(end_time - start_time))
-        print("\n")
-        return map(obfuscate, parsed_generic_text)
+        return result
