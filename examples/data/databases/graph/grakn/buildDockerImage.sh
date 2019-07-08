42dfb21d47532224f9f0ed308aae4471a632faf1
mani <sadhak001@gmail.com>
2019-07-08 17:23:13 +0100

    Updating versions of GraalVM and Grakn to use to build docker image for Grakn/GraalVM

--- examples/data/databases/graph/grakn/buildDockerImage.sh
+++ examples/data/databases/graph/grakn/buildDockerImage.sh
@@ -4,7 +4,8 @@ set -e
 set -u
 set -o pipefail
 
-GRAKN_VERSION=${GRAKN_VERSION:-1.5.2}
+GRAKN_VERSION=${GRAKN_VERSION:-1.5.7}
+GRAALVM_VERSION=${GRAALVM_VERSION:-19.1.0}
 
 if [[ -z ${DOCKER_USER_NAME:-""} ]]; then
   read -p "Docker username (must exist on Docker Hub): " DOCKER_USER_NAME
@@ -14,4 +15,9 @@ IMAGE_NAME=${IMAGE_NAME:-grakn}
 IMAGE_VERSION=${IMAGE_VERSION:-${GRAKN_VERSION}}
 GRAKN_DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
 
-time docker build -t ${GRAKN_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION} .
+echo "* Fetching docker image ${GRAKN_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION} from Docker Hub"
+time docker pull ${GRAKN_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION} || true
+time docker build -t ${GRAKN_DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION} \
+                  --build-arg GRAKN_VERSION=${GRAKN_VERSION} \
+                  --build-arg GRAALVM_VERSION=${GRAALVM_VERSION} \
+                  .
