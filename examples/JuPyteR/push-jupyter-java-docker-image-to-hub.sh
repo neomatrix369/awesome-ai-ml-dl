e0e4d3414c69e31f03a8941574aef3510242cc48
mani <sadhak001@gmail.com>
2019-09-08 08:59:06 +0100

    Updating the push image to hub script

--- examples/JuPyteR/push-jupyter-java-docker-image-to-hub.sh
+++ examples/JuPyteR/push-jupyter-java-docker-image-to-hub.sh
@@ -8,29 +8,28 @@ if [[ -z ${DOCKER_USER_NAME:-""} ]]; then
   read -p "Docker username (must exist on Docker Hub): " DOCKER_USER_NAME
 fi
 
-IMAGE_NAME=${IMAGE_NAME:-jupyter-java}
-IMAGE_VERSION=${IMAGE_VERSION:-$(cat version.txt)}
-DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
-
 findImage() {
 	IMAGE_NAME=$1
 	echo $(docker images ${IMAGE_NAME} -q | head -n1 || true)
 }
 
-IMAGE_FOUND="$(findImage ${DOCKER_USER_NAME}/${IMAGE_NAME})"
-if [[ -z "${IMAGE_FOUND}" ]]; then
-    echo "Docker image '${DOCKER_USER_NAME}/${IMAGE_NAME}' not found in the local repository"
-    IMAGE_FOUND="$(findImage ${IMAGE_NAME})"
-    if [[ -z "${IMAGE_FOUND}" ]]; then
-    	echo "Docker image '${IMAGE_NAME}' not found in the local repository"
-    	exit 1
-    else 
-    	echo "Docker image '${IMAGE_NAME}' found in the local repository"
-    fi
-else
-    echo "Docker image '${DOCKER_USER_NAME}/${IMAGE_NAME}' found in the local repository"
-fi
+pushImage() {
+    language_id=$1
+    IMAGE_NAME=${IMAGE_NAME:-jupyter-java}
+    IMAGE_VERSION=$(cat version.txt)
+    DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
 
-docker tag ${IMAGE_FOUND} ${DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION}
+    IMAGE_FOUND="$(findImage ${DOCKER_FULL_TAG_NAME})"
+    IS_FOUND="found"
+    if [[ -z "${IMAGE_FOUND}" ]]; then
+        IS_FOUND="not found"        
+    fi
+    echo "Docker image '${DOCKER_USER_NAME}/${IMAGE_NAME}' is ${IS_FOUND} in the local repository"
+
+    docker tag ${IMAGE_FOUND} ${DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION}
+    docker push ${DOCKER_FULL_TAG_NAME}
+}
+
 docker login --username=${DOCKER_USER_NAME}
-docker push ${DOCKER_FULL_TAG_NAME}
+pushImage base java-base
+pushImage ${1:-java}
