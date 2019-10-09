0a8b2b96a76a4b9c53b57f952318833a0d7624e3
mani <sadhak001@gmail.com>
2019-10-09 21:16:51 +0100

    Better NLP: Updating the library code and the dependent classes

--- examples/better-nlp/library/org/neomatrix369/better_nlp.py
+++ examples/better-nlp/library/org/neomatrix369/better_nlp.py
@@ -1,43 +1,89 @@
-import spacy             # $ pip install spacy
+"""
+BetterNLP is a wrapper program/library that encapsulates a couple
+of NLP libraries that are popular among the AI and ML communities.
+"""
+#
+# Copyright 2019 Mani Sarkar
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+### Please feel free to fork this work as long as you maintain the above
+### license and provide citation.
+
+import os
 import time
-import textacy.extract   # $ pip install textacy
+import struct
 import pandas as pd      # $ pip install pandas
 
-from org.neomatrix369.summariser_cosine import SummariserCosine
-from org.neomatrix369.summariser_pytextrank import SummariserPyTextRank
+print("This version of Python is {} bits.".format(8 * struct.calcsize("P")))
+
+### These two imports must stay here till we have installed them using
+### pip install in the above block of code
+### Note: you will see a number of 'pylint: disable...' across the
+### code snippet to disable false positives or not relevant warnings
+### thrown by pylint
+# pylint: disable=C0413
+import spacy             # $ pip install spacy
+import textacy.extract   # $ pip install textacy
+
+from org.neomatrix369.summariser_cosine          import SummariserCosine
+from org.neomatrix369.summariser_pytextrank      import SummariserPyTextRank
+from org.neomatrix369.summariser_tfidf           import SummariserTFIDF
+from org.neomatrix369.summariser_tfidf_variation import SummariserTFIDFVariation
 
 
 MINIMUM_NUM_OF_WORDS = 1
 MINIMUM_OCCURRENCE_FREQUENCY = 1
 
-
 def obfuscate(token, entity_type="PERSON"):
+    """
+      Obfuscates a given token depending on its type,
+      default is PERSON type
+    """
     if token.string.strip() not in ['', '\n', '\t']:
         if token.ent_iob != 0 and token.ent_type_ == entity_type:
             return "[OBFUSCATED] "
-        else:
-            return token.string
-    else:
-        return ''
+        return token.string
+    return ''
 
 
 class BetterNLP:
+    """
+      BetterNLP orchestrates all the NLP related functions
+      contained in this source file
+    """
 
     minimum_occurrence_frequency = MINIMUM_OCCURRENCE_FREQUENCY
 
     english_nlp_model = None
 
     model_loading_time = 0
-    
+
     def load_nlp_model(self):
-        # Ensure you do the below two steps before loading the English NLP Model:
-        #
-        # 	$ python3 -m spacy download en_core_web_lg
-        # 	$ python3 -m spacy link en_core_web_lg en
-        #
+        """
+        Loads the NLP model in a lazy manner
+
+        Ensure you do the below two steps before loading
+        the English NLP Model:
+
+           $ python3 -m spacy download en_core_web_lg
+           $ python3 -m spacy link en_core_web_lg en
+
+        """
         nlp_model = 'en'
 
-        if self.english_nlp_model is None: 
+        if self.english_nlp_model is None:
             print("Loading model '{}'...".format(nlp_model))
             start_time = time.time()
             self.english_nlp_model = spacy.load(nlp_model)
@@ -54,7 +100,12 @@ class BetterNLP:
         result["model"] = self.english_nlp_model
         return result
 
+    # pylint: disable=R0201
     def token_entity_types(self):
+        """
+          Returns the legend of all the entity types,
+          both abbreviations and their meaning
+        """
         return {
             'Entity type':[
                 'PERSON',
@@ -97,18 +148,31 @@ class BetterNLP:
                 'Numerals that do not fall under another type'
             ]
         }
-    
+
+    # pylint: disable=R0201
     def pretty_print(self, data, max_colwidth=500):
+        """
+          Pretty prints a dataframe,
+          default max col width is 500
+        """
         pd.set_option('display.max_colwidth', max_colwidth)
         print(pd.DataFrame(data))
 
+    # pylint: disable=R0201
     def extract_entities(self, model, text):
+        """
+          Extract entities using the model,
+          from the text passed in
+        """
         start_time = time.time()
+        # pylint: disable=E1102
         parsed_text = model(text)
 
         data = {
-            "text": [each_entity.text for each_entity in parsed_text.ents if each_entity.text.strip() == each_entity.text],
-            "label": [each_entity.label_ for each_entity in parsed_text.ents if each_entity.text.strip() == each_entity.text]
+            "text": [each_entity.text for each_entity in parsed_text.ents \
+                           if each_entity.text.strip() == each_entity.text],
+            "label": [each_entity.label_ for each_entity in parsed_text.ents \
+                           if each_entity.text.strip() == each_entity.text]
         }
         duration = time.time() - start_time
 
@@ -119,36 +183,71 @@ class BetterNLP:
         return result
 
     def parts_of_speech_tagging(self, model, text):
+        """
+          Extract the parts of speech using the model,
+          from the text passed in
+        """
+        # pylint: disable=E1121
         parsed_generic_text = self.extract_entities(model, text).get("parsed_text")
 
         start_time = time.time()
-        parts_of_speech_tagging = {'token': [token.text for token in parsed_generic_text], 
-                'lemma': [token.lemma_ for token in parsed_generic_text],
-                'parts-of-speech': [token.pos_ for token in parsed_generic_text],
-                'tag': [token.tag_ for token in parsed_generic_text],
-                'syntactic dependency': [token.dep_ for token in parsed_generic_text],
-                'shape': [token.shape_ for token in parsed_generic_text],        
-                'is_alphanumeric': [token.is_alpha for token in parsed_generic_text],
-                'is_stop_word': [token.is_stop for token in parsed_generic_text]}
+        parts_of_speech_tagging = \
+             {
+                 'token': [
+                     token.text for token in parsed_generic_text
+                 ],
+                 'lemma': [
+                     token.lemma_ for token in parsed_generic_text
+                 ],
+                 'parts-of-speech': [
+                     token.pos_ for token in parsed_generic_text
+                 ],
+                 'tag': [
+                     token.tag_ for token in parsed_generic_text
+                 ],
+                 'syntactic dependency': [
+                     token.dep_ for token in parsed_generic_text
+                 ],
+                 'shape': [
+                     token.shape_ for token in parsed_generic_text
+                 ],
+                 'is_alphanumeric': [
+                     token.is_alpha for token in parsed_generic_text
+                 ],
+                 'is_stop_word': [
+                     token.is_stop for token in parsed_generic_text
+                 ]
+             }
         duration = time.time() - start_time
 
         result = {}
         result["parsed_text"] = parsed_generic_text
         result["parts_of_speech"] = parts_of_speech_tagging
         result["parts_of_speech_processing_time_in_secs"] = duration
-        return result                
+        return result
 
     def extract_noun_chunks(self, model, text):
+        """
+          Extract noun chunks using the model,
+          from the text passed in
+        """
+        # pylint: disable=E1121
         parsed_generic_text = self.extract_entities(model, text)
         parsed_generic_text = parsed_generic_text.get("parsed_text")
         start_time = time.time()
-        noun_chunks = textacy.extract.noun_chunks(parsed_generic_text, min_freq=self.minimum_occurrence_frequency)
+        noun_chunks = textacy.extract.noun_chunks( \
+                         parsed_generic_text, \
+                         min_freq=self.minimum_occurrence_frequency)
         noun_chunks = map(str, noun_chunks)
         noun_chunks = map(str.lower, noun_chunks)
 
         set_of_noun_chunks = set(noun_chunks)
 
-        data = {'Words belonging together (lowercase)': [each_noun_chunk for each_noun_chunk in set_of_noun_chunks if len(each_noun_chunk.split(" ")) > self.minimum_occurrence_frequency]}
+        data = {'Words belonging together (lowercase)':
+                [each_noun_chunk for each_noun_chunk in
+                 set_of_noun_chunks \
+                         if len(each_noun_chunk.split(" ")) > \
+                                   self.minimum_occurrence_frequency]}
         duration = time.time() - start_time
 
         result = {}
@@ -157,30 +256,52 @@ class BetterNLP:
         result["noun_chunks_processing_time_in_secs"] = duration
         return result
 
-    def extract_facts(self, model, text, target_topic):
-        start_time = time.time()
-        parsed_generic_text = self.extract_entities(model, text)
-        parsed_generic_text = parsed_generic_text.get("parsed_text")
-        facts = textacy.extract.semistructured_statements(parsed_generic_text, target_topic)
-
-        list_of_facts=[]
+    # pylint: disable=R0201
+    def get_facts(self, facts, target_topic):
+        """
+          Extract facts for the target topic from
+          the passed in data structure
+        """
+        list_of_facts = []
         for each_fact_statement in facts:
+            # pylint: disable=W0612
             subject, verb, fact = each_fact_statement
             list_of_facts.append(fact.text)
 
-        data = {
+        result = {
             'Facts about ' + target_topic: list_of_facts
         }
+
+        return result
+
+    def extract_facts(self, model, text, target_topic):
+        """
+          Extract facts using the model,
+          from the text passed in
+        """
+        start_time = time.time()
+        # pylint: disable=E1121
+        parsed_generic_text = (self.extract_entities(model, text)
+                               .get("parsed_text"))
+        facts = textacy.extract.semistructured_statements( \
+                                parsed_generic_text, target_topic)
+
+        list_of_facts = self.get_facts(facts, target_topic)
 
         duration = time.time() - start_time
 
         result = {}
         result["parsed_text"] = parsed_generic_text
-        result["facts"] = data
+        result["facts"] = list_of_facts
         result["extract_facts_processing_time_in_secs"] = duration
         return result
 
     def obfuscate_text(self, model, text):
+        """
+          Obfuscate the entities using the model,
+          in the text passed in
+        """
+        # pylint: disable=E1121
         parsed_generic_text = self.extract_entities(model, text)
         parsed_generic_text = parsed_generic_text.get("parsed_text")
 
@@ -188,7 +309,7 @@ class BetterNLP:
         for each_entity in parsed_generic_text.ents:
             each_entity.merge()
         duration = time.time() - start_time
-        
+
         result = {}
         result["parsed_text"] = parsed_generic_text
         result["obfuscate_text_processing_time_in_secs"] = duration
@@ -197,20 +318,36 @@ class BetterNLP:
         return result
 
     def summarise(self, text, method="cosine", top_n_sentences=5):
+        """
+          Summarise a block of text and return a gist
+          of the original text along with metadata.
+
+          Process different types of summarisers: 3 supported
+             SummariserCosine
+             SummariserPyTextRank
+             SummariserTFIDF
+             SummariserTFIDFVariation
+        """
         start_time = time.time()
 
         result = {}
         if method == "cosine":
             summariser = SummariserCosine()
-            result["summarised_text"], result["ranked_sentences"] = summariser.generate_summary(text, top_n_sentences)
+            result["summarised_text"], result["ranked_sentences"] = \
+                        summariser.generate_summary(text, top_n_sentences)
         elif method == "pytextrank":
-            from org.neomatrix369.summariser_pytextrank import SummariserPyTextRank
             summariser = SummariserPyTextRank()
-            result["summarised_text"], result["token_ranks"], result["key_phrases"], result["graph"] = summariser.generate_summary(text, top_n_sentences)
-        elif method == "nltk_sklearn":
-            from org.neomatrix369.summariser_nltk_sklearn import SummariserNltkSklearn
-            summariser = SummariserNltkSklearn()
-            result["summarised_text"], result["important_words"] = summariser.generate_summary(text, top_n_sentences)
+            result["summarised_text"], result["token_ranks"], \
+                     result["key_phrases"], result["graph"] = \
+                        summariser.generate_summary(text, top_n_sentences)
+        elif method == "tfidf":
+            summariser = SummariserTFIDF()
+            result["summarised_text"], result["important_words"] = \
+                        summariser.generate_summary(text, top_n_sentences)
+        elif method == "tfidf-ignore-stopwords":
+            summariser = SummariserTFIDFVariation()
+            result["summarised_text"], result["scored_documents"] = \
+                        summariser.generate_summary(text, top_n_sentences)
 
         duration = time.time() - start_time
         result["summarisation_processing_time_in_secs"] = duration
@@ -218,6 +355,10 @@ class BetterNLP:
         return result
 
     def show_graph(self, graph, method="pytextrank"):
+        """
+          Return a graph of the vertices when using
+          the SummariserPyTextRank summeriser
+        """
         if method == "pytextrank":
             summariser = SummariserPyTextRank()
             summariser.show_graph(graph)
