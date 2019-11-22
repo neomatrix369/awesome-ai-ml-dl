ae773376c37eefd036c00b7cbaa94ba3a3e65b9b
mani <sadhak001@gmail.com>
2019-11-22 18:24:46 +0000

    Apache Zeppelin example: push image to docker hub after building image successfully

--- examples/apache-zeppelin/buildZeppelinDockerImage.sh
+++ examples/apache-zeppelin/buildZeppelinDockerImage.sh
@@ -17,8 +17,9 @@ else
              -t ${DOCKER_FULL_TAG_NAME}:${IMAGE_VERSION} \
              --build-arg ZEPPELIN_VERSION=0.8.1          \
              --build-arg SPARK_VERSION=2.4.4             \
-             --build-arg GRAALVM_VERSION=19.2.0.1        \
+             --build-arg GRAALVM_VERSION=19.3.0          \
              -f Zeppelin-Dockerfile .
 fi
 
 ./removeUnusedContainersAndImages.sh
+./push-apache-zeppelin-docker-image-to-hub.sh
