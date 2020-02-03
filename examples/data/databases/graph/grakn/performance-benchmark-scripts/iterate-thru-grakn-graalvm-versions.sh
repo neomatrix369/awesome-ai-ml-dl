f0a66bff7b36f3b9557cbcf8a5cf543622fd3e25
mani <sadhak001@gmail.com>
2020-02-03 16:53:28 +0000

    Capturing output from each run into a well-named output file

--- examples/data/databases/graph/grakn/performance-benchmark-scripts/iterate-thru-grakn-graalvm-versions.sh
+++ examples/data/databases/graph/grakn/performance-benchmark-scripts/iterate-thru-grakn-graalvm-versions.sh
@@ -57,15 +57,21 @@ do
 		if [[ "$(isVersionGreaterThanOrEqualTo "${GRAALVM_VERSION}" "19.3.0")" = "true" ]]; then
 			for GRAALVM_JDK_VERSION in ${GRAALVM_JDK_VERSIONS[@]}
 			do
+				LOGFILE="${grakn}-${GRAKN_VERSION}-graalvm-ce-${GRAALVM_JDK_VERSION}-${GRAALVM_VERSION}-startup-times.md"
 				GRAALVM_VERSION=${GRAALVM_VERSION}         \
 				GRAALVM_JDK_VERSION=${GRAALVM_JDK_VERSION} \
 				GRAKN_VERSION=${GRAKN_VERSION}             \
-				     ./measureTradVersusGraalVMStartupTime.sh
+				     ./measureTradVersusGraalVMStartupTime.sh &> "${LOGFILE}"
+				cat "${LOGFILE}"
+				echo "Output saved in ${LOGFILE}"
 			done
 		else
+			LOGFILE="${grakn}-${GRAKN_VERSION}-graalvm-ce-${GRAALVM_VERSION}-startup-times.md"
 			GRAALVM_VERSION=${GRAALVM_VERSION} \
 			GRAKN_VERSION=${GRAKN_VERSION}     \
-			     ./measureTradVersusGraalVMStartupTime.sh
+			     ./measureTradVersusGraalVMStartupTime.sh &> "${LOGFILE}"
+			cat "${LOGFILE}"
+			echo "Output saved in ${LOGFILE}"
         fi
 	done
 done
